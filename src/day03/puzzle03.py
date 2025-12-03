2a4f0b0c75e89d538a569575f69ad8b3352bfd86
Craig Trader <craig.trader@gmail.com>
2025-12-03 04:50:43 -0500

    Day 03

--- src/day03/puzzle03.py
+++ src/day03/puzzle03.py
@@ -10,11 +10,21 @@ class Day03(Puzzle):
         return self.read_stripped(filename)
 
     def part1(self, data: Data) -> PuzzleResult:
-        return 0
+        return sum(self.rating(batteries, 2) for batteries in data)
 
     def part2(self, data: Data) -> PuzzleResult:
-        return 0
+        return sum(self.rating(batteries, 12) for batteries in data)
+
+    @staticmethod
+    def rating(batteries: str, count: int) -> int:
+        value = 0
+        pos = 0
+        for end in range(len(batteries)-count+1, len(batteries)+1):
+            biggest: str = max(batteries[pos:end])
+            pos: int = batteries.index(biggest, pos, end) + 1
+            value: int = value * 10 + int(biggest)
+        return value
 
 
 puzzle = Day03()
-puzzle.run()
+puzzle.run(357, 3121910778619)
