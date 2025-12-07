0ac1df551691c68112c20d46229f00b241687df9
Craig Trader <craig.trader@gmail.com>
2025-12-07 09:33:10 -0500

    Day 07

--- src/day07/puzzle07.py
+++ src/day07/puzzle07.py
@@ -1,20 +1,74 @@
 from __future__ import annotations
 
 from common import *
+from collections import deque
+
+
+SPLITTER = '^'
+PATH = '|'
+
+
+class Tachyon:
+
+    def __init__(self, lines):
+        self.manifold = Grid(lines, sparse=True, conversion=Grid.conv_blank('.'))
+
+    def tachyon_manifold(self) -> int:
+        splits = 0
+        queue: deque[GridPosition] = deque([self.manifold.first])
+        while len(queue):
+            next: GridPosition = queue.popleft() + DOWN
+            if next in self.manifold and self.manifold[next] == SPLITTER:
+                split = False
+
+                left: GridPosition = next+LEFT
+                if left not in self.manifold:
+                    self.manifold[left] = PATH
+                    queue.append(left)
+                    split = True
+
+                right: GridPosition = next+RIGHT
+                if right not in self.manifold:
+                    self.manifold[right] = PATH
+                    queue.append(right)
+                    split = True
+
+                if split:
+                    splits += 1
+
+            elif GridRow(next) <= self.manifold.rows:
+                if next not in self.manifold:
+                    self.manifold[next] = PATH
+                    queue.append(next)
+
+        return splits
+
+    @cache
+    def count_paths(self, position) -> int:
+        while position in self.manifold and self.manifold[position] != SPLITTER:
+            position += DOWN
+
+        if self.manifold[position] == SPLITTER:
+            return self.count_paths(position+LEFT)+self.count_paths(position+RIGHT)
+        else:
+            return 1
+
+    def quantum_tachyon_manifold(self) -> int:
+        return self.count_paths(self.manifold.first)
 
 
 class Day07(Puzzle):
-    """Solution for day 07"""
+    """Solution for day 07 (Laboratories)"""
 
     def parse_data(self, filename: str) -> Data:
-        return self.read_stripped(filename)
+        return self.read_factory(filename, Tachyon)
 
-    def part1(self, data: Data) -> PuzzleResult:
-        return 0
+    def part1(self, data: Tachyon) -> PuzzleResult:
+        return data.tachyon_manifold()
 
-    def part2(self, data: Data) -> PuzzleResult:
-        return 0
+    def part2(self, data: Tachyon) -> PuzzleResult:
+        return data.quantum_tachyon_manifold()
 
 
 puzzle = Day07()
-puzzle.run()
+puzzle.run(21, 40)
