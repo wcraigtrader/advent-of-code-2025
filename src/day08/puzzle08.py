53b7850440458ede349da3f47184536e05b5a63c
W. Craig Trader <github@trader.name>
2025-12-09 01:19:11 +0000

    Day 08

--- src/day08/puzzle08.py
+++ src/day08/puzzle08.py
@@ -1,13 +1,42 @@
 from __future__ import annotations
 
+from math import sqrt
 from common import *
+
+
+@dataclass
+class JunctionBox:
+    x: int
+    y: int
+    z: int
+
+    def distance(self, other: JunctionBox) -> float:
+        return sqrt(
+            (other.x - self.x ^ 2) + (other.y - self.y ^ 2) + (other.z - self.z) ^ 2
+        )
+
+    @classmethod
+    def factory(cls, line) -> JunctionBox:
+        x, y, z = line.split(',')
+        return cls(int(x), int(y), int(z))
+
+
+@dataclass
+class Distance:
+    link: tuple[int, int]
+    boxes: 
+
+class Playground:
+
+    def __init__(self, lines):
+        self.boxes: list[JunctionBox] = [JunctionBox.factory(line) for line in lines]
 
 
 class Day08(Puzzle):
     """Solution for day 08 (Playground)"""
 
     def parse_data(self, filename: str) -> Data:
-        return self.read_stripped(filename)
+        return self.read_factory_lines(filename, Playground)
 
     def part1(self, data: Data) -> PuzzleResult:
         return 0
@@ -17,4 +46,4 @@ class Day08(Puzzle):
 
 
 puzzle = Day08()
-puzzle.run()
+puzzle.run(40)
